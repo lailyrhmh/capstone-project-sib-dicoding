d1f28b01b54f8c09790b53a4a53a7f9eb6c7f240
Novia Aryanti <niputunovia@gmail.com>
2022-12-08 09:44:33 +0800

    memperbaiki ML

--- views.py
+++ views.py
@@ -1,14 +1,10 @@
 from flask import Blueprint, render_template, abort, request
-
-views = Blueprint(__name__, "views")
-
 import nltk
 nltk.download('wordnet')
 nltk.download('stopwords')
 nltk.download('omw-1.4')
 
 import numpy as np
-# import matplotlib.pyplot as plt
 import pandas as pd
 from nltk.corpus import stopwords
 stop = set(stopwords.words('english'))
@@ -16,8 +12,6 @@ stop.update(['.', ',', '"', "'", '?','|','!', ':', ';', '(', ')', '[', ']', '{',
 from nltk.stem import WordNetLemmatizer
 
 df = pd.read_csv('./dataset/Daylio_Abid.csv')
-
-new_df = df.dropna(axis = 0, how ='any')
 
 new_df = df.drop(['full_date', 'date', 'time', 'weekday'], axis=1)
 
@@ -40,14 +34,10 @@ def search_activities(sub_mood):
 
 def activities_recommendation(sub_mood):
     topn = []
-    topn = search_activities(sub_mood) #function create dictionary only for particular mood
-    print("5 Recommendation Activities in %s are:"%(sub_mood))
-    # print(topn[0])
-    # print(topn[1])
-    # print(topn[2])
-    # print(topn[3])
-    # print(topn[4])
+    topn = search_activities(sub_mood) 
     return topn[:5]
+
+views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
@@ -57,13 +47,13 @@ def home():
 def formMood():
     return render_template("form-mood.html")
 
-@views.route("/recommendation-activity", methods=['GET'])
+@views.route("/recommendation-activity")
 def recommendation():
     return render_template("activity-recomendation.html")
 
-@views.route('/recommendation-activity/result')
+@views.route('/find',  methods=['GET'])
 def get_activities():
     sub_mood = request.args.get('sub_mood')
     result = activities_recommendation(sub_mood)
-    result_str = 'You should eat {}, {}, or {}.'.format(result[0], result[1], result[2])
-    return render_template('activity-result.html', result=result_str, sub_mood=sub_mood)
+    
+    return render_template('activity-result.html', sub_mood = sub_mood, result1 = result[0], result2 = result[1], result3 = result[2], result4 = result[3], result5 = result[4])
